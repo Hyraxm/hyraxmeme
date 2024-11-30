ed9f8f875f92d12d68d70b82285dd7f15e4c7cb6
Hyraxm <hyrax.meme@outlook.com>
2024-11-30 01:11:08 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('buy-now').addEventListener('click', () => {
+    alert('Redirecting you to PancakeSwap/Uniswap...');
+    // Here, you can add a real redirection link
+    window.location.href = 'https://jup.ag';
+});
