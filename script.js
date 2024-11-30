8bdcd2ef78f3f0dd197c68ad02d9f49af9e503ba
Hyraxm <hyrax.meme@outlook.com>
2024-11-30 01:28:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('buy-now').addEventListener('click', () => {
-    alert('Redirecting you to PancakeSwap/Uniswap...');
+    alert('Redirecting you to Jupiter...');
     // Here, you can add a real redirection link
     window.location.href = 'https://jup.ag';
 });
